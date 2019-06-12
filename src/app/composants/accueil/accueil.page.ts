8f653e1616958f5a506a2344bf71ddcf4231b5c2
Hugo Combe <hugo.co42@gmail.com>
2019-06-12 16:29:43 +0200

    Ajout articles page accueil

--- src/app/composants/accueil/accueil.page.ts
+++ src/app/composants/accueil/accueil.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/model/Article';
 
 @Component({
   selector: 'app-accueil',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['accueil.page.scss']
 })
 export class AccueilPage {
+
+  articles = [
+    new Article("1","Article1","Description1","PouikPouik","Voiture","12/12/2012","Chamboeuf", "12,78"),
+    new Article("2","Article2","Description2","PouikPouik","Voiture","12/12/2012","Chamboeuf", "12,78"),
+    new Article("3","Article3","Description3","PouikPouik","Voiture","12/12/2012","Chamboeuf", "12,78"),
+    new Article("4","Article4","Description4","PouikPouik","Voiture","12/12/2012","Chamboeuf", "12,78"),
+    new Article("5","Article5","Description5","PouikPouik","Voiture","12/12/2012","Chamboeuf", "12,78"),
+  ]
 
   constructor() {
     
