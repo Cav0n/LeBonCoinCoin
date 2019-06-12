0abc77b87eaba291cedf0f11b5381f05ff1d70c4
Florian Bernard <Cav0n@hotmail.fr>
2019-06-12 16:28:39 +0200

    Added User class

---
+++ src/model/User.ts
@@ -0,0 +1,11 @@
+import { firestore } from 'firebase';
+
+export class User {
+    id: string;
+    username: string;
+
+    constructor(id: string, username: string){
+        this.id = id;
+        this.username = username;
+    }
+}
