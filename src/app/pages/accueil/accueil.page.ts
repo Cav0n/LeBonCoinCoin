997f41cb722362d0c29367de242102bcf57f361e
Hugo Combe <hugo.co42@gmail.com>
2019-06-17 17:39:31 +0200

    Merge

--- src/app/pages/accueil/accueil.page.ts
+++ src/app/pages/accueil/accueil.page.ts
@@ -11,11 +11,11 @@ import { NavController } from '@ionic/angular';
 export class AccueilPage {
 
   articles = [
-    new Article('1', 'Article1', 'Description1', 'PouikPouik', 'Voiture', '12/12/2012', 'Chamboeuf', '12,78'),
-    new Article('2', 'Article2', 'Description2', 'PouikPouik', 'Voiture', '12/12/2012', 'Chamboeuf', '12,78'),
-    new Article('3', 'Article3', 'Description3', 'PouikPouik', 'Voiture', '12/12/2012', 'Chamboeuf', '12,78'),
-    new Article('4', 'Article4', 'Description4', 'PouikPouik', 'Voiture', '12/12/2012', 'Chamboeuf', '12,78'),
-    new Article('5', 'Article5', 'Description5', 'PouikPouik', 'Voiture', '12/12/2012', 'Chamboeuf', '12,78'),
+    new Article('1', 'Article1', 'Description1', 'Vendeur1', 'Catégorie1', 'DateDeMiseEnLigne1', 'Ville1', '11,11'),
+    new Article('2', 'Article2', 'Description2', 'Vendeur2', 'Catégorie2', 'DateDeMiseEnLigne2', 'Ville2', '12,12'),
+    new Article('3', 'Article3', 'Description3', 'Vendeur3', 'Catégorie3', 'DateDeMiseEnLigne3', 'Ville3', '13,13'),
+    new Article('4', 'Article4', 'Description4', 'Vendeur4', 'Catégorie4', 'DateDeMiseEnLigne4', 'Ville4', '14,14'),
+    new Article('5', 'Article5', 'Description5', 'Vendeur5', 'Catégorie5', 'DateDeMiseEnLigne5', 'Ville4', '15,15'),
   ];
 
   constructor(public navController: NavController) {  }
--- src/model/Article.ts
+++ src/model/Article.ts
@@ -1,7 +1,15 @@
 import { firestore } from 'firebase';
 
 export class Article {
-    constructor(public id: string, public nom: string, public description: string, public vendeur: string, public categorie: string, public dateDeMiseEnLigne: string, public ville: string, public prix: string){
+    constructor(
+        public id: string, 
+        public nom: string, 
+        public description: string, 
+        public vendeur: string, 
+        public categorie: string, 
+        public dateDeMiseEnLigne: string, 
+        public ville: string, 
+        public prix: string){
 
     }
 }
