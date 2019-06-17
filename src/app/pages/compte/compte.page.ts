88d3a0d7ab22544757ce2e3180b6efee9da3950d
Florian Bernard <Cav0n@hotmail.fr>
2019-06-17 18:08:56 +0200

    Updated profil page code behind to get user (+ route guard fixed)

--- src/app/pages/compte/compte.page.ts
+++ src/app/pages/compte/compte.page.ts
@@ -5,6 +5,7 @@ import * as firebase from 'firebase';
 import { User } from 'src/model/User';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-compte',
@@ -13,19 +14,26 @@ import { Router } from '@angular/router';
 })
 export class ComptePage {
 
-  userName: string;
-  userID: string;
+  user;
+  userID = "";
 
   constructor(
     private userService: UserService,
     private afs: AngularFirestore,
     private authService: AuthenticationService,
     private router: Router) {
-
+      this.userID = firebase.auth().currentUser.uid;
   }
 
   ngOnInit(): void {
-    
+    this.afs.collection('articles').doc<User>(firebase.auth().currentUser.uid).valueChanges().pipe(
+      take(1),
+      map(user => {
+        return user;
+      })
+    ).subscribe(user => {
+      this.user = user;
+    });
   }
 
   logout() {
