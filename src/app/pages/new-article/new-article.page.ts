8d429ad0073e27abe548da725c6699541ab22506
Benjamin Lopes-Vicente <benjaminlopesvicente@gmail.com>
2019-06-18 20:59:05 +0200

    update new article page and code

--- src/app/pages/new-article/new-article.page.ts
+++ src/app/pages/new-article/new-article.page.ts
@@ -1,5 +1,7 @@
 import { Article } from 'src/model/Article';
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from 'src/app/services/toast.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-new-article',
@@ -13,10 +15,39 @@ export class NewArticlePage implements OnInit {
   categorie: string;
   prix: number;
 
-  constructor() {
+  constructor(private toastService: ToastService,
+              private userService: UserService,
+    ) {
   }
 
   ngOnInit() {
+  }
+
+  submit() {
+    const verif = this.checkFields();
+
+    if (!verif.passed) {
+      this.toastService.presentToast(verif.message);
+      return;
+    }
+
+    // todo ajouter l'objet à la collection
+    const article = new Article('unId', this.nom, this.description, this.userService.currentUser, this.categorie, this.prix);
+  }
+
+  checkFields(): {passed: boolean, message: string} {
+    let passed = true;
+    let message = '';
+
+    if (this.prix < 0){
+      passed = false;
+      message = 'le prix doit-être superieur ou égale à 0';
+    } else if ( this.nom == null || this.description == null ||
+                  this.categorie == null || this.prix == null) {
+      passed = false;
+      message = 'Tout les champs doivent être remplis';
+    }
+    return {passed, message};
   }
 
 }
