6fe84076eb72f047a0b4cd3a4b45777af82149c4
Florian Bernard <florian.bernard@etu.uca.fr>
2019-06-20 22:37:57 +0200

    Replacing previous vendeur id from article by real vendeur username (check article.component.ts 28)

--- src/model/Article.ts
+++ src/model/Article.ts
@@ -3,21 +3,21 @@ import { User } from './User';
 
 export class Article {
 
-    ville: string;
     dateDeMiseEnLigne: Date;
 
     constructor(
         public nom: string,
         public description: string,
-        public vendeur: User,
+        public vendeur: string,
         public categorie: string,
-        public prix: number) {
+        public prix: number,
+        public ville: string) {
             this.nom = nom;
             this.description = description;
             this.categorie = categorie;
             this.vendeur = vendeur;
             this.prix = prix;
-            this.ville = vendeur.ville;
+            this.ville = ville;
             this.dateDeMiseEnLigne = new Date();
     }
 }
