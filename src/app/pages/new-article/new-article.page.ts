f2023f87cb7d8ea03a528942494e38ec65384afb
Florian Bernard <cav0n@hotmail.fr>
2019-06-26 14:47:01 +0200

    preparing for image upload for new article

--- src/app/pages/new-article/new-article.page.ts
+++ src/app/pages/new-article/new-article.page.ts
@@ -9,6 +9,8 @@ import * as firebase from 'firebase';
 import { User } from 'src/model/User';
 import { take, map } from 'rxjs/operators';
 import { Categorie, enumSelector } from 'src/model/Categorie';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-new-article',
@@ -20,6 +22,8 @@ export class NewArticlePage implements OnInit {
   description: string;
   categorie: string;
   prix: number;
+  currentImage: any;
+  imageName;
 
   catEnum = enumSelector(Categorie);
 
@@ -29,7 +33,9 @@ export class NewArticlePage implements OnInit {
   constructor(private toastService: ToastService,
               private userService: UserService,
               private afs: AngularFirestore,
-              private navController: NavController
+              private navController: NavController,
+              private camera: Camera,
+              private file: File
     ) {
       this.userID = firebase.auth().currentUser.uid;
   }
@@ -46,19 +52,112 @@ export class NewArticlePage implements OnInit {
     }
 
     const newID = this.afs.createId();
+    if(this.currentImage != null){
+      this.uploadToFirebase(this.currentImage);
 
-    this.afs.collection('articles').doc(newID).set({
-      id: newID,
-      categorie: this.categorie,
-      date: new Date(),
-      description: this.description,
-      nom: this.nom,
-      prix: this.prix,
-      vendeur: (this.userService.currentUser as User).id,
-      ville: (this.userService.currentUser as User).ville
+      this.afs.collection('articles').doc(newID).set({
+        
+        id: newID,
+        categorie: this.categorie,
+        date: new Date(),
+        description: this.description,
+        nom: this.nom,
+        prix: this.prix,
+        vendeur: (this.userService.currentUser as User).id,
+        ville: (this.userService.currentUser as User).ville,
+        image: (this.currentImage.name)
+      });
+
+      this.navController.back();
+    }
+  }
+
+  async pickImage() {
+    const options: CameraOptions = {
+      quality: 80,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    try {
+      let cameraInfo = await this.camera.getPicture(options);
+      let blobInfo = await this.makeFileIntoBlob(cameraInfo);
+      this.imageName = (blobInfo as any).fileName;
+      let uploadInfo: any = await this.uploadToFirebase(blobInfo);
+
+      alert("File Upload Success " + uploadInfo.fileName);
+    } catch (e) {
+      console.log(e.message);
+      alert("File Upload Error " + e.message);
+    }
+  }
+
+  // FILE STUFF
+  makeFileIntoBlob(_imagePath) {
+    // INSTALL PLUGIN - cordova plugin add cordova-plugin-file
+    return new Promise((resolve, reject) => {
+      let fileName = "";
+      this.file
+        .resolveLocalFilesystemUrl(_imagePath)
+        .then(fileEntry => {
+          let { name, nativeURL } = fileEntry;
+
+          // get the path..
+          let path = nativeURL.substring(0, nativeURL.lastIndexOf("/"));
+          console.log("path", path);
+          console.log("fileName", name);
+
+          fileName = name;
+
+          // we are provided the name, so now read the file into
+          // a buffer
+          return this.file.readAsArrayBuffer(path, name);
+        })
+        .then(buffer => {
+          // get the buffer and make a blob to be saved
+          let imgBlob = new Blob([buffer], {
+            type: "image/jpeg"
+          });
+          console.log(imgBlob.type, imgBlob.size);
+          resolve({
+            fileName,
+            imgBlob
+          });
+        })
+        .catch(e => reject(e));
     });
+  }
 
-    this.navController.back();
+  /**
+   *
+   * @param _imageBlobInfo
+   */
+  uploadToFirebase(_imageBlobInfo) {
+    console.log("uploadToFirebase");
+    return new Promise((resolve, reject) => {
+      let fileRef = firebase.storage().ref("images/" + _imageBlobInfo.fileName);
+
+      let uploadTask = fileRef.put(_imageBlobInfo.imgBlob);
+
+      uploadTask.on(
+        "state_changed",
+        (_snapshot: any) => {
+          console.log(
+            "snapshot progess " +
+              (_snapshot.bytesTransferred / _snapshot.totalBytes) * 100
+          );
+        },
+        _error => {
+          console.log(_error);
+          reject(_error);
+        },
+        () => {
+          // completion...
+          resolve(uploadTask.snapshot);
+        }
+      );
+    });
   }
 
   checkFields(): {passed: boolean, message: string} {
